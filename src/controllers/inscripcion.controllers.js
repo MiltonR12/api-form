475d18d2e18e0d53e67c5350b6be0b848d118dce
MiltonR12 <109563498+MiltonR12@users.noreply.github.com>
2023-01-13 18:17:55 -0400

    first commit

--- src/controllers/inscripcion.controllers.js
+++ src/controllers/inscripcion.controllers.js
@@ -1,34 +1,70 @@
 import { pool } from "../db.js";
 
 export const getInscripciones = async (req, res) => {
-  const [result] = await pool.query('SELECT * FROM inscripcion')
-  res.json(result)
+  try {
+    const [result] = await pool.query('SELECT * FROM inscripcion')
+    res.json(result)
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message
+    })
+  }
 }
 
 export const getInscripcion = async (req, res) => {
-  const [result] = await pool.query('SELECT * FROM inscripcion WHERE id = ?', [req.params.id])
-  res.json(result[0])
+  try {
+    const [result] = await pool.query('SELECT * FROM inscripcion WHERE id = ?',
+      [req.params.id])
+    res.json(result[0])
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message
+    })
+  }
 }
 
 export const addInscripcion = async (req, res) => {
   const { name, number, integrantes, nicks, logo, comprobante } = req.body;
-  const [result] = await pool.query('INSERT INTO inscripcion (name, number, integrantes, nicks, logo, comprobante) VALUES (?,?,?,?,?,?)', [name, number, integrantes, nicks, logo, comprobante]);
-  res.json({
-    id: result.insertId,
-    name,
-    integrantes,
-    nicks,
-    logo,
-    comprobante
-  })
+  try {
+    const [result] = await pool.query('INSERT INTO inscripcion (name, number, integrantes, nicks, logo, comprobante) VALUES (?,?,?,?,?,?)', [name, number, integrantes, nicks, logo, comprobante]);
+    res.json({
+      id: result.insertId,
+      name,
+      integrantes,
+      nicks,
+      logo,
+      comprobante
+    })
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message
+    })
+  }
 }
 
 export const deleteInscripcion = async (req, res) => {
-  const result = await pool.query('DELETE FROM inscripcion WHERE id = ?', [req.params.id])
-  res.send(result)
+  try {
+    const result = await pool.query('DELETE FROM inscripcion WHERE id = ?', [req.params.id])
+    if (result.affectedRows == 0) {
+      return res.status(404).json({
+        message: 'Equipo not found'
+      })
+    }
+    return res.send(result)
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message
+    })
+  }
 }
 
 export const updateInscripcion = async (req, res) => {
-  const [result] = await pool.query('UPDATE inscripcion SET ? WHERE id = ?', [req.body, req.params.id])
-  res.json(result)
+  try {
+    const [result] = await pool.query('UPDATE inscripcion SET ? WHERE id = ?', [req.body, req.params.id])
+    res.json(result)
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message
+    })
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { PORT } from "./config.js";
 import routerInscripcion from "./routers/inscripcion.routes.js";
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 app.use('/api', routerInscripcion)
 
